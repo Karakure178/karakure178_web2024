79e9d5a37edda51a57b9615be0fdb8cc42f3b55a
Karakure178 <egg9017shun@gmail.com>
2024-01-03 22:16:06 +0900

    add - inner記述追加

--- src/components/page/top/introduction/Intro.vue
+++ src/components/page/top/introduction/Intro.vue
@@ -4,7 +4,7 @@
   <section class="introduction">
     <div class="introduction__inner">
       <h1 class="introduction__name">Karakure178</h1>
-      <p class="introduction__job">Frontend<br />engineer</p>
+      <p class="introduction__job">Frontend<br />engineer.</p>
     </div>
   </section>
 </template>
