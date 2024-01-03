bf26a8d77a6c39905a9d773d7752880a0421f4a6
Karakure178 <egg9017shun@gmail.com>
2024-01-04 00:21:28 +0900

    add - scrollnavの実装

---
+++ src/components/ui/scroll/scrollNav/ScrollNav.vue
@@ -0,0 +1,30 @@
+<script setup>
+const sectionNav = defineProps({
+  sections: {
+    type: Object,
+    required: true,
+  },
+});
+
+console.log(sectionNav.sections);
+</script>
+
+<template>
+  <div class="scrollNav">
+    <ul class="scrollNav__list">
+      <li
+        v-for="item in sectionNav.sections"
+        :key="item.id"
+        class="scrollNav__item"
+      >
+        <a class="scrollNavItem__link" :href="item.href">
+          <span></span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "./ScrollNav.scss";
+</style>
--- src/views/top/Top.vue
+++ src/views/top/Top.vue
@@ -1,19 +1,24 @@
 <script setup>
 import { useHead } from "unhead";
+import { ref } from "vue";
 
 import Introduction from "./../../components/page/top/introduction/Intro.vue";
-import { headData } from "./data.js";
+import ScrollNav from "./../../components/ui/scroll/scrollNav/ScrollNav.vue";
+import { headData, topData } from "./data.js";
 
 useHead({
   title: headData.title,
   meta: [{ property: "og:title", content: headData.title }],
   link: [{ rel: "stylesheet", href: headData.font }],
 });
+
+const sectionList = ref(topData.scrollNav);
 </script>
 
 <template>
   <main>
     <Introduction />
+    <ScrollNav :sections="sectionList" />
   </main>
 </template>
 
