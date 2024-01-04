b9a7297036e6ef56fc7f5477095252680685c306
Karakure178 <egg9017shun@gmail.com>
2024-01-04 11:58:33 +0900

    add - 自分のアイコン追加(縮小治ってない)

--- src/components/page/top/about/About.vue
+++ src/components/page/top/about/About.vue
@@ -1,16 +1,26 @@
 <script setup>
+import { ref } from "vue";
+
 import Links from "./Links.vue";
+
+// ref使って別物として書かないとエラーを吐き続けたので分けた
+const myicon = ref("src/assets/image/top/about/myicon.png");
 </script>
 
 <template>
   <section id="about" class="about">
     <div class="about__inner">
-      <h2 class="about__heading">About</h2>
-      <dl class="about__text">
-        <dt class="aboutText__likes">Likes</dt>
-        <dd class="aboutText__likesExplanation">Creative coding</dd>
-        <Links />
-      </dl>
+      <div class="about__explantion">
+        <h2 class="about__heading">About</h2>
+        <dl class="about__text">
+          <dt class="aboutText__likes">Likes</dt>
+          <dd class="aboutText__likesExplanation">Creative coding</dd>
+          <Links />
+        </dl>
+      </div>
+      <div class="about__icon">
+        <img :src="myicon" alt="自分のicon" />
+      </div>
     </div>
   </section>
 </template>
