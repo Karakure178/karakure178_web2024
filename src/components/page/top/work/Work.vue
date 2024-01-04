cbe9c8b6d7c20282f8db679044150d8b8848854d
Karakure178 <egg9017shun@gmail.com>
2024-01-04 13:02:55 +0900

    fix - レイアウト修正

--- src/components/page/top/work/Work.vue
+++ src/components/page/top/work/Work.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from "vue";
+
+import Explantion from "./Explantion.vue";
 </script>
 
 <template>
@@ -7,12 +9,7 @@ import { ref } from "vue";
     <div class="work__inner">
       <h2 class="work__heading">Work</h2>
       <div class="work__carousel">
-        <dl class="work__explantion">
-          <dt class="workExplantion__title">Title:</dt>
-          <dd class="workExplantion__titleText">Colorful shapes</dd>
-          <dt class="workExplantion__date">Date:</dt>
-          <dd class="workExplantion__dateText">2023-12-10</dd>
-        </dl>
+        <Explantion />
         <!-- TODO 要修正 共通コンポーネントから引っ張り出す -->
         <button class="work__codeButton">read code</button>
         <div class="work__image">
