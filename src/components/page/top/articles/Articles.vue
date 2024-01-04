7cfecc5d85637e0308832342eccbfa336a597cf5
Karakure178 <egg9017shun@gmail.com>
2024-01-04 15:56:09 +0900

    add - articlesセクションの追加

---
+++ src/components/page/top/articles/Articles.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from "vue";
+
+import Article from "./Article.vue";
+import { workData } from "./data.js";
+
+const items = ref(workData.work);
+const commonUrl = ref(workData.commonUrl);
+</script>
+
+<template>
+  <section id="articles" class="articles">
+    <div class="articles__inner">
+      <h2 class="articles__heading">Articles</h2>
+      <div v-for="item in items" :key="item.id" class="work__carousel">
+        <Article :title="item.title" :date="item.date" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use "./Articles.scss";
+</style>
---
+++ src/components/page/top/articles/data.js
@@ -0,0 +1,54 @@
+export const articlesData = {
+  commonUrl: "src/assets/image/common/icon/",
+  link: [
+    {
+      href: "https://zenn.dev/karakure178/articles/9fc0fb59b6577d",
+      title: "[p5.js]業務知識をフル活用したローカル開発環境を紹介(2023年度版)",
+      date: "2023/12/30",
+      src: "zenn.svg",
+      alt: "zenn link",
+    },
+    {
+      href: "https://zenn.dev/karakure178/articles/994e26c60eef30",
+      title: "p5.jsとGLSLを使ったポストエフェクトが楽しすぎる！",
+      date: "2023/12/04",
+      src: "zenn.svg",
+      alt: "zenn link",
+    },
+    {
+      href: "https://zenn.dev/karakure178/articles/26ba7ca0251c4e",
+      title: "[Pixi.JS×pixi3d]Canvasで3dモデルをくり抜いて動画マスクにしたい",
+      date: "2023/11/19",
+      src: "zenn.svg",
+      alt: "zenn link",
+    },
+    {
+      href: "https://zenn.dev/karakure178/articles/26ba7ca0251c4e",
+      title: "[Pixi.JS×pixi3d]Canvasで3dモデルをくり抜いて動画マスクにしたい",
+      date: "2023/11/19",
+      src: "zenn.svg",
+      alt: "zenn link",
+    },
+    {
+      href: "https://zenn.dev/karakure178/articles/eb0e60557c8ecb",
+      title: "[Obsidian] Dataviewjsを使ってその日に記録したURL一覧を表示する",
+      date: "2023/08/13",
+      src: "zenn.svg",
+      alt: "zenn link",
+    },
+    {
+      href: "https://zenn.dev/karakure178/articles/7f409a9474403d",
+      title: "新卒エンジニアとして3か月働いてできないこと",
+      date: "2023/07/22",
+      src: "zenn.svg",
+      alt: "zenn link",
+    },
+    {
+      href: "https://zenn.dev/karakure178/articles/0c8dde26fb15cb",
+      title: "デイリーコーディングイベント「minacoding」を完走した",
+      date: "2023/07/04",
+      src: "zenn.svg",
+      alt: "zenn link",
+    },
+  ],
+};
