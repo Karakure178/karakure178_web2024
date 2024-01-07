23f5a37518099d99d830376adf76a2296d3ce881
Karakure178 <egg9017shun@gmail.com>
2024-01-08 02:50:36 +0900

    add - swiperを使った処理を実装

--- src/components/page/top/work/Work.vue
+++ src/components/page/top/work/Work.vue
@@ -1,6 +1,10 @@
 <script setup>
+import { register } from "swiper/element/bundle";
 import { ref } from "vue";
 
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import ReadButton from "../../../ui/button/common/CommonButton.vue";
 
 import Explantion from "./Explantion.vue";
@@ -8,23 +12,42 @@ import { workData } from "./data.js";
 
 const items = ref(workData.work);
 const commonUrl = ref(workData.commonUrl);
+
+register();
 </script>
 
 <template>
   <section id="work" class="work">
     <div class="work__inner">
       <h2 class="work__heading">Work</h2>
-      <div v-for="item in items" :key="item.id" class="work__carousel">
-        <div class="work__image">
-          <img :src="commonUrl + item.src" :alt="item.alt" />
-        </div>
-        <Explantion :title="item.title" :date="item.date" />
-        <ReadButton
-          :href="item.href"
-          class-name="work__button"
-          button-text="read code"
-        />
-      </div>
+      <swiper-container
+        :pagination="{
+          hideOnClick: false,
+        }"
+        :speed="2000"
+        :loop="true"
+        :autoplay="{
+          delay: 3000, // 1.5秒後に次のスライド
+        }"
+        class="mySwiper"
+      >
+        <swiper-slide
+          v-for="(item, index) in items"
+          :key="item.id"
+          class="work__carousel"
+          :virtual-index="index"
+        >
+          <div class="work__image">
+            <img :src="commonUrl + item.src" :alt="item.alt" />
+          </div>
+          <Explantion :title="item.title" :date="item.date" />
+          <ReadButton
+            :href="item.href"
+            class-name="work__button"
+            button-text="read code"
+          />
+        </swiper-slide>
+      </swiper-container>
     </div>
   </section>
 </template>
--- src/components/page/top/work/data.js
+++ src/components/page/top/work/data.js
@@ -8,5 +8,12 @@ export const workData = {
       src: "work-colorful.png",
       alt: "OpenProcessing link",
     },
+    {
+      title: "Colorful shapes!",
+      date: "2023/12/10",
+      href: "https://openprocessing.org/sketch/2120354",
+      src: "work-colorful.png",
+      alt: "OpenProcessing link",
+    },
   ],
 };
